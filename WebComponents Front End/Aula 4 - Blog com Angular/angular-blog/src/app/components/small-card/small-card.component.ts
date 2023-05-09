7bb0bbb97b40df224fdd9dff53ef126a493fb78c
pablo.carvalho <pablo.carvalho@icommgroup.com.br>
2023-05-09 18:44:12 -0300

    Class: Finishing Angular blog

--- WebComponents Front End/Aula 4 - Blog com Angular/angular-blog/src/app/components/small-card/small-card.component.ts
+++ WebComponents Front End/Aula 4 - Blog com Angular/angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css', './small-card.responsive.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  photoAlt:string = ""
+  @Input()
+  photoAuthor:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardLink:string = ""
+  @Input()
+  Id:string = ''
 
   constructor() { }
 
